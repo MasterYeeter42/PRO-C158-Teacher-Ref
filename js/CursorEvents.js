d2618026b539052d8516d772004cfca7c7144748
MasterYeeter42 <108326655+MasterYeeter42@users.noreply.github.com>
2024-02-14 17:49:07 +0000

    Update CursorEvents.js

--- js/CursorEvents.js
+++ js/CursorEvents.js
@@ -3,9 +3,35 @@ AFRAME.registerComponent("cursor-listener", {
     selectedItemId: { default: "", type: "string" },
   },
   init: function () {
+    this.handleClickEvents();
     this.handleMouseEnterEvents();
     this.handleMouseLeaveEvents();
   },
+  handleClickEvents: function () {
+    //BÕB
+    this.el.addEventListener("click", evt => {
+      const placesContainer = document.querySelector("#places-container");
+      const { state } = placesContainer.getAttribute("tour");
+
+      if (state === "places-list") {
+        const id = this.el.getAttribute("id");
+        const placesId = [
+          "taj-mahal",
+          "budapest",
+          "naw-york-city",
+          "eiffel-tower"
+        ];
+        if (placesId.includes(id)) {
+          placesContainer.setAttribute("tour", {
+            state: "view",
+            selectedCard: id
+          });
+        }
+      }
+    });
+  },
+
+    
   handlePlacesListState: function () {
     const id = this.el.getAttribute("id");
     const placesId = ["taj-mahal", "budapest", "new-york-city", "eiffel-tower"];
